035574f4fdda0c2fa96879629888ec0bf89ff1ef
Duda <137117671+duds0901@users.noreply.github.com>
2024-08-30 09:50:33 -0300

    "first commit"

---
+++ API_02_DUDA/functions.js
@@ -0,0 +1,16 @@
+const { timeStamp } = require("console");
+const { message } = require("statuses");
+
+function response(sts, msg, aftrows, data = null){
+    return {
+        status: sts, 
+        message: msg,
+        affected_rows: aftrows,
+        data: data,
+        timeStamp: new Date().getTime()
+    }
+}
+
+module.exports = {
+    response
+}
